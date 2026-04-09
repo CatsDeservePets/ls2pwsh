d4702f1954fbe1d7c58302a3936c570931ac0702
CatsDeservePets <145048791+CatsDeservePets@users.noreply.github.com>
2026-04-09 18:30:35 +0200

    feat: support reverse conversion from `PSStyle.FileInfo`

--- main.go
+++ main.go
@@ -13,17 +13,31 @@ import (
 
 var progName = strings.TrimSuffix(filepath.Base(os.Args[0]), ".exe")
 
-type style string
-
-func (s style) String() string {
-	return "\"`e[" + string(s) + "m\""
+type fileInfo struct {
+	directory    string
+	symbolicLink string
+	executable   string
+	extension    map[string]string
 }
 
-type fileInfo struct {
-	directory    style
-	symbolicLink style
-	executable   style
-	extension    map[string]style
+type format byte
+
+const (
+	unknown format = iota
+	pwsh
+	gnu
+	// TODO: bsd?
+)
+
+func (f format) String() string {
+	switch f {
+	case pwsh:
+		return "pwsh"
+	case gnu:
+		return "gnu"
+	default:
+		return "unknown"
+	}
 }
 
 const help = `usage: %s LS_COLORS
@@ -45,8 +59,17 @@ func main() {
 	if err != nil {
 		flag.Usage()
 	}
-	fi := fromLSCOLORS(args)
-	fmt.Println(fi.toPWSH())
+	switch detectFormat(args) {
+	case gnu:
+		fi := fromLSCOLORS(args)
+		fmt.Println(fi.toPSStyle())
+	case pwsh:
+		fi := fromPSStyle(args)
+		fmt.Println(fi.toLSCOLORS())
+	default:
+		fmt.Fprintf(os.Stderr, "%s: unrecognized input format\n", progName)
+		os.Exit(1)
+	}
 }
 
 func readInput(args []string, f *os.File) (string, error) {
@@ -65,28 +88,81 @@ func readInput(args []string, f *os.File) (string, error) {
 	return "", fmt.Errorf("no input")
 }
 
+func detectFormat(s string) format {
+	switch {
+	case strings.Contains(s, "di="), strings.Contains(s, "ln="), strings.Contains(s, "ex="):
+		return gnu
+	case strings.Contains(s, "Directory"), strings.Contains(s, "SymbolicLink"), strings.Contains(s, "Executable"), strings.Contains(s, "Extension"):
+		return pwsh
+	default:
+		return unknown
+	}
+}
+
+func fromPSStyle(s string) fileInfo {
+	var fi fileInfo
+
+	unescape := func(s string) string {
+		return strings.TrimSuffix(strings.TrimPrefix(strings.Trim(s, "\""), "`e["), "m")
+	}
+
+	for ent := range strings.Lines(s) {
+		ent = strings.TrimSpace(strings.ReplaceAll(ent, " ", ""))
+		k, v, _ := strings.Cut(ent, ":")
+		v = unescape(v)
+		switch k {
+		case "Directory":
+			fi.directory = v
+		case "SymbolicLink":
+			fi.symbolicLink = v
+		case "Executable":
+			fi.executable = v
+		case "Extension":
+			// The first pair appears after 'Extension:' on the same line.
+			k = v
+			fallthrough
+		default:
+			ext, val, found := strings.Cut(k, "=")
+			if !found {
+				continue
+			}
+			if fi.extension == nil {
+				fi.extension = make(map[string]string)
+			}
+			fi.extension[ext] = unescape(val)
+		}
+	}
+
+	return fi
+}
+
 func fromLSCOLORS(s string) fileInfo {
 	var fi fileInfo
+
+	escape := func(s string) string {
+		return "\"`e[" + s + "m\""
+	}
 
 	for ent := range strings.SplitSeq(s, ":") {
 		k, v, found := strings.Cut(ent, "=")
 		if !found {
 			continue
 		}
+		v = escape(v)
 		switch k {
 		case "di":
-			fi.directory = style(v)
+			fi.directory = v
 		case "ln":
-			fi.symbolicLink = style(v)
+			fi.symbolicLink = v
 		case "ex":
-			fi.executable = style(v)
+			fi.executable = v
 		default:
 			if after, ok := strings.CutPrefix(k, "*."); ok {
 				ext := "." + after
 				if fi.extension == nil {
-					fi.extension = make(map[string]style)
+					fi.extension = make(map[string]string)
 				}
-				fi.extension[ext] = style(v)
+				fi.extension[ext] = v
 			}
 		}
 	}
@@ -94,7 +170,7 @@ func fromLSCOLORS(s string) fileInfo {
 	return fi
 }
 
-func (fi fileInfo) toPWSH() string {
+func (fi fileInfo) toPSStyle() string {
 	var b strings.Builder
 
 	if fi.directory != "" {
@@ -109,6 +185,27 @@ func (fi fileInfo) toPWSH() string {
 	if len(fi.extension) != 0 {
 		for k, v := range fi.extension {
 			fmt.Fprintf(&b, "$PSStyle.FileInfo.Extension['%s'] = %s\n", k, v)
+		}
+	}
+
+	return b.String()
+}
+
+func (fi fileInfo) toLSCOLORS() string {
+	var b strings.Builder
+
+	if fi.directory != "" {
+		fmt.Fprintf(&b, "di=%s:", fi.directory)
+	}
+	if fi.symbolicLink != "" {
+		fmt.Fprintf(&b, "ln=%s:", fi.symbolicLink)
+	}
+	if fi.executable != "" {
+		fmt.Fprintf(&b, "ex=%s:", fi.executable)
+	}
+	if len(fi.extension) != 0 {
+		for k, v := range fi.extension {
+			fmt.Fprintf(&b, "%s=%s:", k, v)
 		}
 	}
 
